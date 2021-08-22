176637bfe6e6f1805700b22107297227fd8e6ba9
ericwafula <eric.wafula@moringaschool.com>
2021-08-22 10:39:48 +0300

    Added name getter

--- src/main/java/models/Employee.java
+++ src/main/java/models/Employee.java
@@ -1,7 +1,10 @@
 package models;
 
 public class Employee extends User{
-
+    private String position;
+    private String role;
+    private String department;
+    private int id;
 
     public Employee(String name, String position, String role, String department) {
         super(name);
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -2,17 +2,12 @@ package models;
 
 public class User {
     public String name;
-    private String position;
-    private String role;
-    private String department;
-    private int id;
 
     public User(String name) {
         this.name = name;
-        this.position = position;
-        this.role = role;
-        this.department = department;
     }
 
-
+    public String getName() {
+        return name;
+    }
 }
