96afc5413161380f2b732f58aa4d344f5f564c93
ericwafula <eric.wafula@moringaschool.com>
2021-08-22 16:29:32 +0300

    Added database rule file

---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,22 @@
+import db.DB;
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource{
+    @Override
+    public void before(){
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/news_portal_test", "eric", "1234");
+    }
+
+    @Override
+    public void after(){
+        try(Connection conn = DB.sql2o.open()){
+            String deleteUserQuery = "DELETE FROM users";
+            String deleteDepartmentsQuery = "DELETE FROM departments";
+            String deleteNewsQuery = "DELETE FROM news";
+            conn.createQuery(deleteUserQuery).executeUpdate();
+            conn.createQuery(deleteDepartmentsQuery).executeUpdate();
+            conn.createQuery(deleteNewsQuery).executeUpdate();
+        }
+    }
+}
