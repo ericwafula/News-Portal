22df9e06f894ebf71f7770a34aada8697832cb0c
ericwafula <eric.wafula@moringaschool.com>
2021-08-22 10:30:59 +0300

    Added User object instantiation test

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,3 +1,14 @@
+import models.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class UserTest {
+
+    @Test
+    public void user_instantiatesCorrectly_true() {
+        User user = new User("Eric", "Manager","Manage day to day operations", "IT");
+        assertTrue(user instanceof User);
+    }
+
 
 }
