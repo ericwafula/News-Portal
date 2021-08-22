bf10d21bce0fb3d47a5fa3c6ca2892359274b9b4
ericwafula <eric.wafula@moringaschool.com>
2021-08-22 12:13:27 +0300

    Department instantiates correctly

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,5 +1,12 @@
 package models;
 
 public class Department {
+    private String name;
+    private String description;
+    private int employee_count;
 
+    public Department(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 }
--- src/test/java/DepartmentTest.java
+++ src/test/java/DepartmentTest.java
@@ -3,5 +3,9 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class DepartmentTest {
-
+    @Test
+    public void department_instantiatesCorrectly_true() {
+        Department department1 = new Department("IT", "Focuses on IT related Stuff");
+        assertTrue(department1 instanceof Department);
+    }
 }
